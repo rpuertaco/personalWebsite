5d92e28178aba07c7a168f09754d320d36fbf975
Ruben Puerta Cordoba <>
2024-09-21 21:31:38 +0200

    delete server

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
-import {DAYS} from "../../../assets/data/contant";
+import {DAYS} from "../../shared/constants/contant";
 
 @Component({
   selector: 'app-layout',
@@ -11,13 +11,17 @@ import {DAYS} from "../../../assets/data/contant";
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   selectedBackground: string = 'code';
-  localDate = this.generateDate(new Date());
+  localDate = null;
 
-
+ngOnInit() {
+ setInterval(() => {
+this.localDate = this.generateDate(new Date())
+ }, 1000)
+}
 
   generateDate(date: Date) : any {
-    return DAYS[date.getDay()] + ' ' + date.getDate() + '.' + (date.getMonth() + 1) + '. ' + date.getHours() + ':' + date.getMinutes();
+    return DAYS[date.getDay() - 1] + ' ' + date.getDate() + '. ' + (date.getMonth() + 1) + '. ' + date.getHours() + ':' + date.getMinutes();
   }
 }
