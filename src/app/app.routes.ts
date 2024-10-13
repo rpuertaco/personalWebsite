8a93697116421b453b0ae6a9690f6d6357819303
Ruben Puerta Cordoba <>
2024-10-13 20:13:37 +0200

    about component and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {LayoutComponent} from "./core/layout/layout.component";
-import {WallpaperComponent} from "./core/wallpaper/wallpaper.component";
+import {WallpaperComponent} from "./core/components/wallpaper/wallpaper.component";
 
 export const routes: Routes = [{
     path: '',
     component: LayoutComponent,
     children: [
       {
-      path: '',
-      component: HomeComponent
-    }]
-  },
+        path: 'about',
+        loadComponent: ()=>  import('./core/components/about/about.component').then((c)=>c.AboutComponent )
+      }
+    ]
+  }
 
 ];
