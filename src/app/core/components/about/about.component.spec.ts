2521ddb1b5413b7009ff75758b88ec485b905f77
Ruben Puerta Cordoba <>
2024-11-03 21:15:08 +0100

    set testing

--- src/app/core/components/about/about.component.spec.ts
+++ src/app/core/components/about/about.component.spec.ts
@@ -1,23 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
-import { AboutComponent } from './about.component';
+import {AboutComponent} from "./about.component";
+import {ActiveWindowService} from "../../services/active-window.service";
 
-describe('AboutComponent', () => {
-  let component: AboutComponent;
-  let fixture: ComponentFixture<AboutComponent>;
+describe("AboutComponent", () => {
+    let component: AboutComponent;
+    let fixture: ComponentFixture<AboutComponent>;
+    let windowServiceSpy: jasmine.SpyObj<ActiveWindowService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AboutComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        const spy = jasmine.createSpyObj("ActiveWindowService", ["addWindow", "removeWindow"]);
+        await TestBed.configureTestingModule({
+            imports: [AboutComponent],
+            providers: [
+                {provide: ActiveWindowService, useValue: spy},
+            ]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(AboutComponent);
+        component = fixture.componentInstance;
+        windowServiceSpy = TestBed.inject(ActiveWindowService) as jasmine.SpyObj<ActiveWindowService>;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("should year be current year", () => {
+        const currentYear = new Date().getFullYear();
+        expect(component.year).toBe(currentYear);
+    });
+
+    it("should goToSetting() call activeWindowService.addWindow with value \"settings\"", () => {
+        component.goToSettings();
+        expect(windowServiceSpy.addWindow).toHaveBeenCalledWith("settings");
+    });
+
+    it("should removeWindow call activeWindowService.removeWindow with the value \"about\"", () => {
+        component.close();
+        expect(windowServiceSpy.removeWindow).toHaveBeenCalledWith("about");
+    });
+
 });
--- src/app/core/components/main-navbar/components/nav-dropdown/nav-dropdown.component.spec.ts
+++ src/app/core/components/main-navbar/components/nav-dropdown/nav-dropdown.component.spec.ts
@@ -1,23 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
-import { NavDropdownComponent } from './nav-dropdown.component';
+import {NavDropdownComponent} from "./nav-dropdown.component";
+import {ActiveWindowService} from "../../../../services/active-window.service";
 
-describe('NavDropdownComponent', () => {
-  let component: NavDropdownComponent;
-  let fixture: ComponentFixture<NavDropdownComponent>;
+describe("NavDropdownComponent", () => {
+    let component: NavDropdownComponent;
+    let fixture: ComponentFixture<NavDropdownComponent>;
+    let windowServiceSpy: jasmine.SpyObj<ActiveWindowService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NavDropdownComponent]
+    beforeEach(async () => {
+        const spy = jasmine.createSpyObj("ActiveWindowService", ["addWindow", "removeWindow"]);
+        await TestBed.configureTestingModule({
+            imports: [NavDropdownComponent],
+            providers: [
+                {provide: ActiveWindowService, useValue: spy},
+            ]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(NavDropdownComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("should addActiveWindow call activeWindowService", () => {
+        component.addActiveWindow("settings")
+        expect(windowServiceSpy.addWindow).toHaveBeenCalledWith("settings");
     })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(NavDropdownComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
