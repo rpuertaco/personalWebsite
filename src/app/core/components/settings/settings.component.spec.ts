34cf330ef9d74611d19ac8702c17d1f6ab755f96
Ruben Puerta <69591578+rpuertaco@users.noreply.github.com>
2025-11-08 16:31:58 +0100

    coverage 100

--- src/app/core/components/settings/settings.component.spec.ts
+++ src/app/core/components/settings/settings.component.spec.ts
@@ -1,19 +1,26 @@
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 
 import {SettingsComponent} from "./settings.component";
+import {ActiveWindowService} from "../../services/active-window.service";
 
 describe("SettingsComponent", () => {
     let component: SettingsComponent;
     let fixture: ComponentFixture<SettingsComponent>;
+    let windowServiceSpy: jasmine.SpyObj<ActiveWindowService>;
 
     beforeEach(async () => {
+        const spy = jasmine.createSpyObj("ActiveWindowService", ["addWindow", "removeWindow"]);
         await TestBed.configureTestingModule({
-            imports: [SettingsComponent]
+            imports: [SettingsComponent],
+            providers: [
+                {provide: ActiveWindowService, useValue: spy},
+            ]
         })
             .compileComponents();
 
         fixture = TestBed.createComponent(SettingsComponent);
         component = fixture.componentInstance;
+        windowServiceSpy = TestBed.inject(ActiveWindowService) as jasmine.SpyObj<ActiveWindowService>;
         fixture.detectChanges();
     });
 
@@ -25,4 +32,9 @@ describe("SettingsComponent", () => {
         component.setComponent("general")
         expect(component.settingComponent).toEqual("general");
     })
+
+    it("should removeWindow call activeWindowService.removeWindow with the value \"settings\"", () => {
+        component.closeWindow();
+        expect(windowServiceSpy.removeWindow).toHaveBeenCalledWith("settings");
+    });
 });
